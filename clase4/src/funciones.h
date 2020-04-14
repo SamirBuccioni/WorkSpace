2dbbfdf68025f831d54c2afcca542879464aace1
Samir Buccioni <samirbuccioni@gmail.com>
2020-04-14 12:08:07 -0300

    edit1 biblioteca de funciones

---
+++ clase4/src/funciones.h
@@ -0,0 +1,15 @@
+/*
+ * funciones.h
+ *
+ *  Created on: Apr 13, 2020
+ *      Author: samir
+ */
+
+#ifndef FUNCIONES_H_
+#define FUNCIONES_H_
+
+int getInt(char mensaje[]);
+float getFloat(char mensaje[]);
+char getChar(char mensaje[]);
+
+#endif /* FUNCIONES_H_ */
